27003e94b4d18e1c806fc2826eec7c86930cc970
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-06-02 15:20:47 +0200

    Learining to create components

--- obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8e49367572db2b99698ef1f41cf634cb756a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d532afcc441582036e98dfd0a4786097967257")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
