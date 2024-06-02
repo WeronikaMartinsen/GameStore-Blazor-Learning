9a142f4e76dfb9ae4a9beff8ca0e91a2bd358221
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-06-02 14:31:11 +0200

    Configure watch and run

--- obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2181e33898e2af4ffcdeaa55b19d797735a12d88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1848ed90394fb4b67df6e8a08dc338b18ee82e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
