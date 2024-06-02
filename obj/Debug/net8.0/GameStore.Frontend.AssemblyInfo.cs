80d532afcc441582036e98dfd0a4786097967257
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-06-02 15:04:17 +0200

    Learning integrating C# in HTML

--- obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a142f4e76dfb9ae4a9beff8ca0e91a2bd358221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8e49367572db2b99698ef1f41cf634cb756a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
